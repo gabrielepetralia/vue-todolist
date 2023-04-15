8bb26e32aff4e33b95a1733fc48110fc73cfd76a
gabrielepetralia <gabri.petralia03@gmail.com>
2023-04-15 10:48:13 +0200

    [add] header & sidebar

---
+++ assets/js/db.js
@@ -0,0 +1,29 @@
+const categories = [
+  {
+    icon : "fa-house",
+    name : "Casa",
+    numTasks : 0,
+  },
+  {
+    icon : "fa-cart-shopping",
+    name : "Spesa",
+    numTasks : 0,
+  },
+  {
+    icon : "fa-briefcase",
+    name : "Lavoro",
+    numTasks : 0,
+  },
+  {
+    icon : "fa-book",
+    name : "Studio",
+    numTasks : 0,
+  },
+  {
+    icon : "fa-pills",
+    name : "Salute",
+    numTasks : 0,
+  },
+]
+
+export {categories}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 const { createApp } = Vue;
+import {categories} from "./db.js";
 
 createApp({
   data() {
     return {
-      
+      logo  : "fa-solid fa-check-double",
+      title : "ToDoList",
+      categories,
     }
   }
 
